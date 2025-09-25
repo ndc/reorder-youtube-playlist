bd87990dc8d69e32465f110ff1f8ca5a698955c4
Endy Tjahjono <endy.tjahjono@harbers-solutions.com>
2025-09-25 10:46:02 +0700

    Configure vite to use relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    // Use relative base so built index.html references ./assets/... instead of /assets/...
+    base: './',
 })
